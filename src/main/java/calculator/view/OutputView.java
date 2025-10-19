24eda30efa2dfb98b483cc066b9d3faea2d8b490
starday119 <jy8294@naver.com>
2025-10-19 23:47:09 +0900

    docs(view): OutputView 클래스에 메서드별 주석 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -11,14 +11,24 @@ public class OutputView {
         printDoubleResult(result);
     }
 
+    /**
+     * 정수 결과 출력
+     * double 값을 long으로 변환하여 소수점  제거 ex) 5.0 -> 5
+     */
     private void printIntegerResult(final double result) {
         System.out.println(RESULT_PREFIX + (long) result);
     }
-
+    /**
+     * 실수 결과 출력
+     */
     private void printDoubleResult(final double result) {
         System.out.println(RESULT_PREFIX + result);
     }
 
+    /**
+     * double을 long으로 변환했을 때 값이 변하지 않으면 정수로 판별
+     * ex) 5.0 == (long)5.0 -> true
+     */
     private boolean isInteger(final double value) {
         return value == (long) value;
     }
